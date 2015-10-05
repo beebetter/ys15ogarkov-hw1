f92479d3d398e4b95fd1b1eb98c873a6522815e8
Artem Ogarkov <artem.ogarkov@gmail.com>
2015-10-05 18:35:23 +0300

    unfinished 1

--- src/main/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysis.java
@@ -1,52 +1,72 @@
 package ua.yandex.shad.tempseries;
 
+//import java.util.InputMismatchException;
 public class TemperatureSeriesAnalysis {    
 
+	public static final int MIN_TEMPERATURE = -273;
+	private double[] temperatureSeries;
+	
     public TemperatureSeriesAnalysis() {
         
     }
     
-    public TemperatureSeriesAnalysis(double[] temperatureSeries) {
-        
+    public TemperatureSeriesAnalysis(double[] tempSeries) {
+        boolean withinTheRange = true;
+		for (double t : tempSeries) {
+			if (t < MIN_TEMPERATURE)
+				withinTheRange = false;
+		}
+		if (withinTheRange) {
+			temperatureSeries = (double[])tempSeries.clone();
+		}
+				//else throw new InputMismatchException();
     }
     
-    public double average(){        
+    public double average() {   
+		if (temperatureSeries.length == 0)
+			throw new IllegalArgumentException();
         return 0;
     }    
     
-    public double deviation(){
+    public double deviation() {
+		if (temperatureSeries.length == 0)
+			throw new IllegalArgumentException();
         return 0;
     }
     
-    public double min(){
+    public double min() {
+		if (temperatureSeries.length == 0)
+			throw new IllegalArgumentException();
         return 0;
     }
      
-    public double max(){
+    public double max() {
+		if (temperatureSeries.length == 0)
+			throw new IllegalArgumentException();
         return 0;
     }
     
-    public double findTempClosestToZero(){
+    public double findTempClosestToZero() {
         return 0;
     }
     
-    public double findTempClosestToValue(double tempValue){
+    public double findTempClosestToValue(double tempValue) {
         return 0;
 }
     
-    public double[] findTempsLessThen(double tempValue){
+    public double[] findTempsLessThen(double tempValue) {
         return null;
     }
     
-    public double[] findTempsGreaterThen(double tempValue){
+    public double[] findTempsGreaterThen(double tempValue) {
         return null;
     }
     
-    public TempSummaryStatistics summaryStatistics(){
+    public TempSummaryStatistics summaryStatistics() {
         return null;
     }
     
-    public int addTemps(double ... temps){
+    public int addTemps(double ... temps) {
         return 0;
     }
 }
--- src/test/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysisTest.java
+++ src/test/java/ua/yandex/shad/tempseries/TemperatureSeriesAnalysisTest.java
@@ -15,5 +15,43 @@ public class TemperatureSeriesAnalysisTest {
         assertEquals(expResult, actualResult, 0.00001);
         
     }
-    
+	/*@Test(expected = InputMismatchException.class)
+	//@Test(expected = IllegalArgumentException.class)
+    public void testTemperatureSeriesAnalysisFailWhenOutOfRange() {
+        double[] temperatureSeries = {0.0, -274.0, -272.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);        
+    }
+    */
+	@Test(expected = IllegalArgumentException.class)
+    public void testAverageFailOnEmptyList() {
+        double[] temperatureSeries = {};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+		seriesAnalysis.average();   
+		//fail("Fail first time");
+		
+    }
+	@Test(expected = IllegalArgumentException.class)
+    public void testDeviationFailOnEmptyList() {
+        double[] temperatureSeries = {};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+		seriesAnalysis.deviation();   
+		//fail("Fail first time");
+		
+    }
+	@Test(expected = IllegalArgumentException.class)
+    public void testMinFailOnEmptyList() {
+        double[] temperatureSeries = {};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+		seriesAnalysis.min();   
+		//fail("Fail first time");
+		
+    }
+	@Test(expected = IllegalArgumentException.class)
+    public void testMaxFailOnEmptyList() {
+        double[] temperatureSeries = {};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+		seriesAnalysis.max();   
+		//fail("Fail first time");
+		
+    }
 }
